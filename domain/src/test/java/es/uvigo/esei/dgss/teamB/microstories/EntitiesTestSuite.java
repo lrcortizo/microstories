d99f672ac901234a83a3b0500131769f5b52ae1f
srconde <srconde@esei.uvigo.es>
2018-11-06 18:46:03 +0100

    tsk1 Create Entity Story
    Entity Story created with attributes, getters and setters
    All tests completed and setters of Story modified with validations

---
+++ domain/src/test/java/es/uvigo/esei/dgss/teamB/microstories/EntitiesTestSuite.java
@@ -0,0 +1,12 @@
+package es.uvigo.esei.dgss.teamB.microstories;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	StoryTest.class
+})
+public class EntitiesTestSuite {}
+
