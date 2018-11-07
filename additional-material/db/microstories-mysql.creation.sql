9e394c57ea1ecbf2289df871ae6606df734f471f
lrcortizo <lrcortizo@esei.uvigo.es>
2018-11-07 18:21:29 +0100

    tsk1 created sql files
    Added full, creation data, delete and drop sql files in additional material

--- additional-material/db/microstories-mysql.creation.sql
+++ additional-material/db/microstories-mysql.creation.sql
@@ -14,7 +14,7 @@ CREATE TABLE `Story` (
   `title` varchar(100) NOT NULL,
   `text` varchar(1000) NOT NULL,
   `author` varchar(50) NOT NULL,
-  `publicationDate` datetime NOT NULL,
+  `publicationDate` date NOT NULL,
   `genre` varchar(9) NOT NULL,
   `primaryTheme` varchar(14) NOT NULL,
   `secondaryTheme` varchar(14),
@@ -26,15 +26,3 @@ CREATE TABLE `Story` (
 --
 GRANT ALL PRIVILEGES ON dgss1819_teamB_microstories.* TO microstories@localhost IDENTIFIED BY 'microstoriespass';
 FLUSH PRIVILEGES;
-
---
--- Data for table `Story`
---
-INSERT INTO `Story` (title, text, author, publicationDate, 
-genre, primaryTheme, secondaryTheme) VALUES
-	('Microrrelato 1','Texto del microrrelato 1','Autor 1', '2012-06-18',
-	'STORY', 'ADVENTURE','ROMANTIC'),
-	('Microrrelato 2','Texto del microrrelato 2','Autor 2', '2012-06-18',
-	'POETRY', 'SCIENCEFICTION', 'HISTORY'),
-	('Microrrelato 3','Texto del microrrelato 3','Autor 3', '2012-06-18',
-	'NANOSTORY', 'ROMANTIC', 'CHILDREN');
---
+++ additional-material/db/microstories-mysql.delete.sql
@@ -0,0 +1,9 @@
+USE `dgss1819_teamB_microstories`;
+--
+-- Remove data for table `Story`
+--
+DELETE FROM `Story`;
+--
+-- Reset AUTO_INCREMENT for table `Story`
+--
+ALTER TABLE `Story` AUTO_INCREMENT = 1;
