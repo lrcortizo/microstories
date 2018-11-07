e528e0241a7e1c4fbefc14eda8518e784e398c68
lrcortizo <lrcortizo@esei.uvigo.es>
2018-11-07 18:21:29 +0100

    tsk1 created sql files
    Added full, creation data, delete and drop sql files in additional material

---
+++ additional-material/db/microstories-mysql.data.sql
@@ -0,0 +1,12 @@
+USE `dgss1819_teamB_microstories`;
+--
+-- Data for table `Story`
+--
+INSERT INTO `Story` (title, text, author, publicationDate, 
+genre, primaryTheme, secondaryTheme) VALUES
+	('Microrrelato 1','Texto del microrrelato 1','Autor 1', '2012-06-18',
+	'STORY', 'ADVENTURE','ROMANTIC'),
+	('Microrrelato 2','Texto del microrrelato 2','Autor 2', '2012-06-18',
+	'POETRY', 'SCIENCEFICTION', 'HISTORY'),
+	('Microrrelato 3','Texto del microrrelato 3','Autor 3', '2012-06-18',
+	'NANOSTORY', 'ROMANTIC', 'CHILDREN');
---
+++ additional-material/db/microstories-mysql.drop.sql
@@ -0,0 +1,5 @@
+USE `dgss1819_teamB_microstories`;
+--
+-- Remove table `Story`
+--
+DROP TABLE IF EXISTS `Story`;
