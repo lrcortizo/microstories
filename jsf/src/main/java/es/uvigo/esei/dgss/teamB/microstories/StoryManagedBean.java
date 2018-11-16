918569d04c26c320e6125ba6f2ac079876fe2925
lrcortizo <lrcortizo@esei.uvigo.es>
2018-11-16 22:20:46 +0100

    task6 Recent stories list in index.xhtml
    Creates jsf for recent stories list with a maximum of 6 and order by publication date

---
+++ jsf/src/main/java/es/uvigo/esei/dgss/teamB/microstories/StoryManagedBean.java
@@ -0,0 +1,18 @@
+package es.uvigo.esei.dgss.teamB.microstories;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.inject.Named;
+
+@Named("index")
+public class StoryManagedBean {
+	
+	@EJB
+	private StoryEJB storyEJB;
+
+	public List<Story> listRecentStories() {
+		return storyEJB.listRecentStories();
+	}
+
+}
